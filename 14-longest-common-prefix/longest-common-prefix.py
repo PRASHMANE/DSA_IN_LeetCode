df898f2734972ae4d31f4e457082afbbb7c6c667
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2025-12-09 11:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (29.71%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -5,12 +5,9 @@ class Solution:
 
         for s in strs[1:]:
             while pref != s[0:pref_len]:
-                pref_len -= 1
+                pref_len-=1
                 if pref_len == 0:
                     return ""
-                
                 pref = pref[0:pref_len]
-        
         return pref
-
         
