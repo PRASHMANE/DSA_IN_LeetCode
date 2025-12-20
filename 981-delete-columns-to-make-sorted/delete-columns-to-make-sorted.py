23a1519d61f6ac269e73726434162683f87aa85b
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2025-12-20 12:47:54 +0530

    Time: 65 ms (47.13%) | Memory: 18.3 MB (73.28%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -1,13 +1,13 @@
 class Solution:
-    def minDeletionSize(self, strs: List[str]) -> int:
-        count=0
+    def isSortedColumn(self, col: int, strs: list[str]) -> bool:
+        for i in range(1, len(strs)):
+            if strs[i][col] < strs[i - 1][col]:
+                return False
+        return True
+
+    def minDeletionSize(self, strs: list[str]) -> int:
+        count = 0
         for i in range(len(strs[0])):
-            for j in range(len(strs)-1):
-                if ord(strs[j][i]) > ord(strs[j+1][i]):
-                    count+=1
-                    break
+            if not self.isSortedColumn(i, strs):
+                count += 1
         return count
-
-
-
-        
