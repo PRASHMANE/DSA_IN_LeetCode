5323ca21c93894018d46a9f9affa3a06685a08c4
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2025-12-25 19:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (93.61%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -1,31 +1,7 @@
-from collections import Counter
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        l=len(s)
-        n=l-1
-        count= Counter(s)
-        one = count["1"]
-        ans=""
-        if l <= 1:
-            return s
-        if one == 1:
-            for i in range(l):
-                if i != l-1:
-                    ans+="0"
-                else:
-                    ans+="1"
-            return ans 
-        
-        for i in range(l):
-            if one > 1 :
-                ans+="1"
-                one-=1
-            else:
-                if i != l-1:
-                    ans+="0"
-                else:
-                    ans+="1"
-        return ans
+        ones = s.count('1')
+        zeros = len(s) - ones
 
-
-        
+        # (ones - 1) ones at front, zeros in middle, 1 at end
+        return '1' * (ones - 1) + '0' * zeros + '1'
