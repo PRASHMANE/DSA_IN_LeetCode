c964acd2397f52d2c96578a053c23bd404c6d55e
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-01-01 18:40:20 +0530

    Time: 1 ms (46.47%) | Memory: 17.6 MB (85.33%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,14 +1,11 @@
 from collections import Counter
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        hashtable=Counter(arr1)
-        list1=[]
+        Hash=Counter(arr1)
+        ans=[]
         for i in arr2:
-            list1.extend([i]*hashtable[i])
-        
-        arr1.sort()
-        for i in arr1:
-            if i not in list1:
-                list1.extend([i]*hashtable[i])
-        return list1
-        
+            ans.extend([i]*Hash[i])
+            del Hash[i]
+        for i in sorted(Hash.keys()):
+            ans.extend([i]*Hash[i])
+        return ans
