0e27f557e1c91170a33ea09c99a89dc7710f9e3d
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-01-01 15:32:36 +0530

    Time: 84 ms (56.36%) | Memory: 20.1 MB (89.69%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -5,11 +5,9 @@ class Solution:
         if len(nums) <= 2:
             return len(nums)
 
-        k = 2  # first two are always valid
-
-        for i in range(2, len(nums)):
-            if nums[i] != nums[k - 2]:
-                nums[k] = nums[i]
-                k += 1
-
+        k=2
+        for i in range(2,len(nums)):
+            if nums[i] != nums[k-2]:
+                nums[k]=nums[i]
+                k+=1
         return k
