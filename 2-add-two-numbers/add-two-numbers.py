071854d242d111a7aa18a3ed4dec0c47c3bb6404
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-01-12 22:01:57 +0530

    Time: 3 ms (71.99%) | Memory: 19.5 MB (6.24%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -8,21 +8,19 @@ class Solution:
         dummy=ListNode(0)
         curr=dummy
         carry=0
-
         while l1 or l2:
-            x= l1.val if l1  else 0
-            y= l2.val if l2 else 0
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
 
             total = x+y+carry
             carry=total//10
 
-            curr.next=ListNode(total%10)
-            curr=curr.next
-
             if l1: l1 = l1.next
             if l2: l2 = l2.next
 
+            curr.next = ListNode(total%10)
+            curr = curr.next
+
         if carry:
-            curr.next=ListNode(carry)
+            curr.next = ListNode(carry)
         return dummy.next
-        
