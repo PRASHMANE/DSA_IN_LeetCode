7b64c08384c1cd83f4ac4a10f3cd6fc58ce253ee
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-01-30 16:50:58 +0530

    Time: 3 ms (84.96%) | Memory: 21.4 MB (9.51%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,8 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count=Counter(nums)
-        n = float("inf")
-        maxi=0
+        n=len(nums)
         for i in count:
-            if count[i] > maxi :
-                n = i
-                maxi = count[i]
-        return n
+            if count[i] > n//2:
+                return i
                 
