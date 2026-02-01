b136ac7324add575d74fe244aae6dd0cb2fa765e
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-02-01 15:29:22 +0530

    Time: 2 ms (54.34%) | Memory: 20.5 MB (20.13%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash={}
-        for i,j in enumerate(nums):
-            if target-j in hash:
-                return [hash[target-j],i]
-            hash[j]=i
-        return []
-            
+        hsh={}
+        for i,num in enumerate(nums):
+            com = target - num
+            if com in hsh:
+                return [hsh[com],i]
+            hsh[num] = i
+
+
+
         
