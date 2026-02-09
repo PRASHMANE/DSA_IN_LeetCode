397ee1ddce2e33b1b289ec150efc3acbfcad7611
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-02-09 14:11:42 +0530

    Time: 147 ms (90.49%) | Memory: 25.3 MB (54.31%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,21 +1,23 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
 
-        n=len(fruits)
         left=0
+        right=0
+        n= len(fruits)
         hsh={}
         maxlen=0
 
-        for right in range(n):
+        while right < n:
             hsh[fruits[right]]=hsh.get(fruits[right],0)+1
 
-            while len(hsh) > 2:
+            if len(hsh) > 2:
                 hsh[fruits[left]]-=1
-                if hsh[fruits[left]]==0:
+                if hsh[fruits[left]] == 0:
                     del hsh[fruits[left]]
                 left+=1
-            
-            maxlen=max(maxlen,right-left+1)
+            if len(hsh) <= 2:
+                maxlen=max(maxlen,right-left+1)
+            right+=1
         return maxlen
 
         
