3275ab391d1f6ba005a052432d7d046b338edac1
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-02-12 13:41:12 +0530

    Time: 353 ms (36.28%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -4,7 +4,7 @@ FROM SalesPerson s
 WHERE NOT EXISTS (
     SELECT *
     FROM Orders o
-    JOIN Company c ON o.com_id = c.com_id
+    INNER JOIN Company c ON o.com_id = c.com_id
     WHERE o.sales_id = s.sales_id
     AND c.name = 'RED'
 );
