6bdd24705b88458b348b81c3d5ceb356c715957f
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-02-14 10:51:14 +0530

    Time: 56 ms (89.37%) | Memory: 19.8 MB (51.12%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -4,30 +4,35 @@ class Solution:
         if not t or not s:
             return ""
 
-        hsh = {}
-        for c in t:
-            hsh[c] = hsh.get(c, 0) + 1
-
-        left = 0
-        count = 0
         minlen = float("inf")
-        start = 0
+        left=0
+        m=len(t)
+        n=len(s)
+        count=0
 
-        for right in range(len(s)):
+        hsh={}
+
+        for i in t:
+            hsh[i]=hsh.get(i,0)+1
+
+        for right in range(n):
+
             if s[right] in hsh:
-                hsh[s[right]] -= 1
-                if hsh[s[right]] >= 0:
-                    count += 1
+                if hsh[s[right]] > 0:
+                    count+=1
+                hsh[s[right]]=hsh.get(s[right],0)-1
 
-            while count == len(t):
-                if right - left + 1 < minlen:
-                    minlen = right - left + 1
-                    start = left
+            while count == m:
+                if right-left+1 < minlen:
+                    minlen=right-left+1
+                    start=left
 
                 if s[left] in hsh:
-                    hsh[s[left]] += 1
-                    if hsh[s[left]] > 0:
-                        count -= 1
-                left += 1
+                    hsh[s[left]]+=1
 
-        return "" if minlen == float("inf") else s[start:start + minlen]
+                    if hsh[s[left]] > 0:
+                        count-=1
+                left+=1
+        return "" if minlen == float("inf") else s[start:start+minlen]
+
+
