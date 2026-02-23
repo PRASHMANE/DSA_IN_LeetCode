35fe31fb64a3f7d837a825a211a646531fb9624f
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-02-23 20:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (44.53%) - LeetSync

---
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findLUSlength(self, a: str, b: str) -> int:
+        
+        if a == b:
+            return -1
+        
+        if a != b:
+            return max(len(a),len(b))
+
