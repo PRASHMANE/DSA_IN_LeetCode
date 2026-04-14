43f7fd7d8931907d783cfd9321c125566172681d
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-04-14 14:43:58 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (73.96%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -5,19 +5,16 @@ class Solution:
         """
         n = len(nums)
 
-        # Step 1: find breakpoint
-        i = n - 2
-        while i >= 0 and nums[i] >= nums[i + 1]:
-            i -= 1
+        i = n-2
+
+        while(i >= 0 and nums[i] >= nums[i+1]):
+            i-=1
 
         if i >= 0:
-            # Step 2: find next greater
-            j = n - 1
+            j = n-1
             while nums[j] <= nums[i]:
-                j -= 1
-
-            # Step 3: swap
-            nums[i], nums[j] = nums[j], nums[i]
-
-        # Step 4: reverse
-        nums[i + 1:] = reversed(nums[i + 1:])
+                j-=1
+            
+            nums[i],nums[j] = nums[j],nums[i]
+        
+        nums[i+1:] = nums[i+1:][::-1]
