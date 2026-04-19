f8e7f5c81b8589165150d2599672f7796d85f3fd
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-04-20 00:39:48 +0530

    Time: 403 ms (79.22%) | Memory: 33.3 MB (21.21%) - LeetSync

---
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        nums.sort()
+        n = len(nums)
+
+        def can_form_pairs(maxDiff):
+            count = 0
+            i = 0
+            
+            while i < n - 1:
+                if nums[i+1] - nums[i] <= maxDiff:
+                    count += 1
+                    i += 2  # skip both
+                else:
+                    i += 1
+            return count >= p
+
+        left, right = 0, nums[-1] - nums[0]
+
+        while left < right:
+            mid = (left + right) // 2
+            
+            if can_form_pairs(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
