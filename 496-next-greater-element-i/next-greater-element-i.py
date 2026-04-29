e9bd7942e3bde8a353191889c7473b38bb7d0ffd
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-04-29 22:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (68.21%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -2,14 +2,15 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         
         stack = []
-        hsh={}
+        hsh ={}
 
-        for i in nums2:
-            while stack and  i > stack[-1]:
-                hsh[stack.pop()] = i
-            stack.append(i)
+        for num in nums2:
+            while stack and num > stack[-1]:
+                hsh[stack.pop()] = num
+            stack.append(num)
         
         while stack:
             hsh[stack.pop()] = -1
         
-        return [hsh[i] for i in nums1]
+
+        return [hsh[val] for val in nums1]
