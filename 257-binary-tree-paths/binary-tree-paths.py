73df8f0f48a66d32c7c2092735987fe31c051043
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-05-07 12:00:59 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.98%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.py
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -8,21 +8,17 @@ class Solution:
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
 
         res = []
-        
-        def dfs(node, path):
+
+        def dfs(node,path):
             if not node:
                 return
             
-            # If leaf node
             if not node.left and not node.right:
-                res.append(path + str(node.val))
+                res.append(path+str(node.val))
                 return
             
-            # Continue path
-            dfs(node.left, path + str(node.val) + "->")
-            dfs(node.right, path + str(node.val) + "->")
+            dfs(node.left,path+str(node.val)+"->")
+            dfs(node.right,path+str(node.val)+"->")
         
-        dfs(root, "")
+        dfs(root,"")
         return res
-
-        
