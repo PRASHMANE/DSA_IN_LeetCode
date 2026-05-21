7d37f6bf8aaad6780666d1fb5427beb64fe7e515
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-05-21 18:09:31 +0530

    Time: 15 ms (71.26%) | Memory: 20.2 MB (36.46%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -2,37 +2,29 @@ class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
 
         n = len(matrix)
-        m = len(matrix[0])
-
-        ans = float("inf")
-
-        dp = [[None] * m for i in range(n)]
         
+        dp = [[0] * n for _ in range(n)]
 
-        def solve(i,j):
+        for i in range(n):
+            dp[0][i] = matrix[0][i]
 
-            if j < 0 or j > m-1:
-                return float("inf")
+        for i in range(1,n):
+            for j in range(n):
+
+                down = matrix[i][j]+dp[i-1][j]
+
+                if j > 0 :
+                    left = matrix[i][j]+dp[i-1][j-1]
+                else:
+
+                    left = float("inf")
+                
+                if j < n-1:
+                    right = matrix[i][j]+dp[i-1][j+1]
+                else:
+                    right = float("inf")
+                
+
+                dp[i][j] = min(down,left,right)
             
-            if i == n-1:
-                return matrix[i][j]
-            
-            if dp[i][j] != None:
-                return dp[i][j]
-            
-            down = matrix[i][j]+solve(i+1,j)
-            left = matrix[i][j]+solve(i+1,j-1)
-            right = matrix[i][j]+solve(i+1,j+1)
-
-            dp[i][j] = min(down,left,right)
-
-            return dp[i][j]
-
-        for i in range(m):
-            ans = min(ans,solve(0,i))
-        
-        return ans
-            
-            
-
-        
+        return min(dp[-1])
