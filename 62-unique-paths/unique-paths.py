fb469bbd823c9eaaf408af584309dd67f63c1f2b
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-05-23 08:59:35 +0530

    Time: 3 ms (20.11%) | Memory: 19.3 MB (42.11%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -3,13 +3,20 @@ class Solution:
 
         dp = [[-1]*n for _ in range(m)]
 
-        dp[0][0] = 1
+        def solve(i,j):
+            if i == 0 and j==0:
+                return 1
+            
+            if i < 0 or j < 0:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
 
-        for i in range(m):
-            for j in range(n):
-                if i > 0 and j > 0:
-                    up = dp[i-1][j]
-                    left = dp[i][j-1]
-                    dp[i][j] = up+left
+            up = solve(i-1,j)
+            left = solve(i,j-1)
 
-        return abs(dp[m-1][n-1])
+            dp[i][j] = up+left
+            return dp[i][j]
+            
+        return solve(m-1,n-1)
