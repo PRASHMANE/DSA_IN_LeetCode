e1934c0b20e4e4b4bd533d7d9d3e599947169c7e
Prashanth.v <164906048+PRASHMANE@users.noreply.github.com>
2026-05-24 12:17:01 +0530

    Time: 44 ms (76.50%) | Memory: 19.3 MB (90.22%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -9,26 +9,22 @@ class Solution:
         k = (total+target) // 2
         n = len(nums)
 
-        dp = [[-1]*(k+1) for _ in range(n)]
+        dp = [[0]*(k+1) for _ in range(n)]
 
-        def solve(i,t):
-            if i == 0:
-                if t == 0 and nums[0] == 0:
-                    return 2
-                if t == 0 or nums[0] == t:
-                    return 1
-                return 0
-            if dp[i][t] != -1:
-                return dp[i][t]
-
-            not_take = solve(i-1,t)
-            take = 0
-            if nums[i] <= t:
-                take = solve(i-1,t-nums[i])
-            
-            dp[i][t]= take+not_take
-            return dp[i][t]
-        return solve(n-1,k)
-
-        return k
+        if nums[0] == 0:
+            dp[0][0] = 2
+        else:
+            dp[0][0] = 1
         
+        if nums[0] != 0 and nums[0] <= k:
+            dp[0][nums[0]] = 1
+        
+        for i in range(1,n):
+            for t in range(k+1):
+                not_take = dp[i-1][t]
+                take = 0
+                if nums[i] <= t:
+                    take = dp[i-1][t-nums[i]]
+
+                dp[i][t] = take+not_take
+        return dp[n-1][k]
